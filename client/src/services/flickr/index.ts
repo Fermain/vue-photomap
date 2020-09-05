d14a9c83018cbe929f3c131f0bed290fa09e3d08
Oliver Dipple <diplomat@fermain.io>
2020-09-05 17:40:35 +0200

    Added simple service for querying images from geodata

--- client/src/services/flickr/index.ts
+++ client/src/services/flickr/index.ts
@@ -1,3 +1,43 @@
+import { http } from './http';
+import { FlickrPhotoResults, FlickrPhoto } from './photo';
+
 export class FlickrService {
+    public baseUrl = 'https://www.flickr.com/services/rest/';
+
+    private globalConfig = {
+        api_key: process.env.FLICKR_API_KEY,
+        format: 'json',
+        nojsoncallback: '1'
+    }
+
     constructor() {}
+
+    public async search(params: object) {
+        const url = new URL(this.baseUrl);
+        const method = 'flickr.photos.search';
+
+        url.search = new URLSearchParams({
+            ...params,
+            method,
+            ...this.globalConfig
+        }).toString();
+
+        const results = await http<FlickrPhotoResults>(url.toString());
+
+        console.log(results.jsonBody);
+
+        const photos = results.jsonBody?.photos.photo.map((photo: any) => new FlickrPhoto(
+            photo.id,
+            photo.owner,
+            photo.secret,
+            photo.server,
+            photo.farm,
+            photo.title,
+            photo.ispublic,
+            photo.isfriend,
+            photo.isfamily
+        ));
+
+        return photos;
+    }
 }
