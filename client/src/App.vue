45424263d803be26db0a6083f6d3c0034f7deb3c
Oliver Dipple <diplomat@fermain.io>
2020-09-05 13:46:08 +0200

    Paired down and added basic CSS resets

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
 
 @Component({
   components: {
-    HelloWorld,
   },
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+$rem = 16px
+
+html {
+  box-sizing border-box
+  font-size $rem
+}
+
+* {
+  box-sizing inherit
+  &:before, &:after {
+    box-sizing inherit
+  }
+}
+
+body {
+  margin: 0;
+}
 </style>
