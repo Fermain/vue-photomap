015af5ac7ac8a8bafee35fbf0af34d0956f2232e
Oliver Dipple <diplomat@fermain.io>
2020-09-07 09:18:40 +0200

    Added basic dumb components for maps

---
+++ client/src/components/FermainMapSearch.vue
@@ -0,0 +1,11 @@
+<template>
+    <input class="fermain-map-search" type="search" />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class FermainMapSearch extends Vue {
+}
+</script>
