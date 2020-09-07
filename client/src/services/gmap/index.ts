1eb46b7f4c09445b64ff7cd64091324f4dc65ed8
Oliver Dipple <diplomat@fermain.io>
2020-09-07 08:59:51 +0200

    Added autocomplete and gelocation methods

--- client/src/services/gmap/index.ts
+++ client/src/services/gmap/index.ts
@@ -10,13 +10,16 @@ const loader = new Loader(process.env.VUE_APP_GOOGLE_API_KEY, options);
 
 export class GmapService {
     public map!: google.maps.Map;
+    public search!: google.maps.places.Autocomplete;
 
     constructor(
         public targetElement: Element,
         public center: google.maps.LatLngLiteral,
+        public searchElement: HTMLInputElement
     ) {
         loader.load().then(() => {
             this.createMap(center);
+            this.createAutocomplete(searchElement);
         });
     }
 
@@ -25,5 +28,41 @@ export class GmapService {
             center,
             zoom: 8,
         });
+    }
+
+    public createAutocomplete(searchElement: HTMLInputElement) {
+        this.search = new google.maps.places.Autocomplete(searchElement);
+        this.search.bindTo('bounds', this.map);
+        this.search.addListener('place_changed', () => {
+            this.onPlaceSearch();
+        });
+        this.getCurrentLocation();
+    }
+
+    private onPlaceSearch() {
+        const place = this.search.getPlace();
+
+        if (!place.geometry) {
+            window.alert(`Google Maps: '${place.name}' could not be located.`);
+            return;
+        }
+
+        if (place.geometry.viewport) {
+            this.map.fitBounds(place.geometry.viewport);
+        } else {
+            this.map.setCenter(place.geometry.location);
+            this.map.setZoom(17);
+        }
+    }
+
+    public getCurrentLocation() {
+        if(!!navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => { 
+                this.map.setCenter({
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                });
+            });
+        }
     }
 }
