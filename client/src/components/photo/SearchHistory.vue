3a6b1ee59515307b6e0c4fe48784d6282a8cea2d
Oliver Dipple <diplomat@fermain.io>
2020-09-07 14:33:52 +0200

    Added history component

---
+++ client/src/components/photo/SearchHistory.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="history">
+    <div class="history-item" v-for="(value, index) in history" :key="index">
+      {{ value }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import { mapState } from 'vuex';
+
+@Component({
+  computed: mapState(['history']),
+})
+export default class SearchHistory extends Vue {
+}
+</script>
